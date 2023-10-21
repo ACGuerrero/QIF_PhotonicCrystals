a294c9397c0ba7231b9bcced131f0125270e8cf6
ACGuerrero <acastillo@ciencias.unam.mx>
2023-10-21 19:20:06 +0200

    notebook

--- MaxBandGap.py
+++ MaxBandGap.py
@@ -8,12 +8,13 @@ def wavelength_to_pulsation(wl):
 def pulsation_to_wavelength(om):
     return (2*np.pi*c)/(om)
 
-def get_max_bandgap(n1,n2,a,target_puls):
-    wm = 2*np.pi*c*(N1+N2)/(a*4*N1*N2)
+def get_max_bandgap(n1_given,n2_given,a,target_wl_user):
+    wm = 2*np.pi*c*(n1_given+n2_given)/(a*4*n1_given*n2_given)
     l = 2*np.pi*c/wm
-    indzero = np.where((l>(target_central_wl-epsilon)) & (l<(target_central_wl+epsilon))==False)
+    indzero = np.where((l>(target_wl_user-epsilon)) & (l<(target_wl_user+epsilon))==False)
     mask = np.ones(N1.shape)
     mask[indzero] = 0
-    Dw =(4/(np.pi))*np.arcsin(np.abs(N1-N2)/(N1+N2))*target_puls
+    Dw =(4/(np.pi))*np.arcsin(np.abs(n1_given-n2_given)/(n1_given+n2_given))*wavelength_to_pulsation(target_wl_user)
     ind_for_disp = np.where(Dw*mask==np.max(Dw*mask))
-    return (pulsation_to_wavelength(np.max(Dw*mask)), n1[ind_for_disp],n2[ind_for_disp])
+    print(ind_for_disp)
+    return (pulsation_to_wavelength(np.max(Dw*mask)), n1_given[ind_for_disp],n2_given[ind_for_disp])
